6ef6a1930dd65e32fc7cc69e71786df994622dac
Bosung0413 <124684536+NangManBo@users.noreply.github.com>
2025-01-23 14:48:39 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.14 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,13 @@
+def solution(num):
+    n = num
+    answer = 0
+    while(n != 1) :
+        if (n % 2 == 0) :
+            n = n / 2
+        else :
+            n = n * 3 + 1
+        answer += 1
+    
+    if(answer > 500) :
+        answer = -1
+    return answer
