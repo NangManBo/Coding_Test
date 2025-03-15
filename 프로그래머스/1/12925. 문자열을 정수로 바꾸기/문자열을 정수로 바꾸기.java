cfa6793ecc4ae9a809692164fade1d59a4bac046
Bosung0413 <124684536+NangManBo@users.noreply.github.com>
2025-03-15 13:01:51 +0900

    Update 문자열을 정수로 바꾸기.java

--- 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.java
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.java
@@ -1,10 +1,5 @@
 class Solution {
     public int solution(String s) {
-        if(s.charAt(0) != '-'){
-            return Integer.parseInt(s);
-        }
-        else{
-            return Integer.parseInt(s);
-        }
+        return Integer.parseInt(s);
     }
 }
